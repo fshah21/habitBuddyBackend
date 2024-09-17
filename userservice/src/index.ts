562c19d4b3e5dddff8c1b2326f296cbb68fce7fa
feyashah21@gmail.com <apple@APPLEs-MacBook-Air.local>
2024-09-17 23:11:34 +0530

    using fs serv too

--- userservice/src/index.ts
+++ userservice/src/index.ts
@@ -7,12 +7,12 @@ import { matchesRoutes } from './routes/matches.routes';
 const http = require("http");
 import dotenv from 'dotenv';
 import * as fs from 'fs';
+dotenv.config();
 console.log("GOOGLE APPLICATION CREDENTIALS", process.env.GOOGLE_APPLICATION_CREDENTIALS);
 const serviceAccount = JSON.parse(fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS, 'utf8'));
 console.log("SERVICE ACCOUNT", serviceAccount);
 // import { server, io } from './socket';
 
-dotenv.config();
 const admin = require('firebase-admin');
 
 admin.initializeApp({
